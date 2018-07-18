560916a39ab52f846b0555026d6df118eddef5ee
montygoldy <montygoldy2006@gmail.com>
2018-07-18 17:51:31 -0400

    Complete app

--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ const cors = require('cors');
 const path = require('path');
 
 // Routes import
-const users = require('./server/routes/users');
-const employees = require('./server/routes/employees');
-const feedbacks = require('./server/routes/feedbacks');
+const users = require('./routes/users');
+const employees = require('./routes/employees');
+const feedbacks = require('./routes/feedbacks');
 
 const app = express();
 
 // Db config
-const db = require('./server/config/keys').mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to mongodb
 mongoose
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use(passport.initialize());
-require('./server/services/passport')(passport);
+require('./services/passport');
 
 // Routes
 app.use('/api/users', users);
@@ -40,10 +40,10 @@ app.use('/api/feedbacks', feedbacks);
 // Server Static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('../client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
 
